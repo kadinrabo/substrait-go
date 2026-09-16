9355de4c8ddf5fc2ed09ee06a393c1b86290ec5a
Kadin Rabo <kadinjrabo@gmail.com>
2026-09-16 17:37:48 -0400

    refactor(expr)!: move WindowFunction encoding to wire
    BREAKING CHANGE: WindowFunction encoding is removed from core; encode via wire.

--- expr/functions.go
+++ expr/functions.go
@@ -524,6 +524,7 @@ func (w *WindowFunction) SessionDependant() bool                  { return w.dec
 func (w *WindowFunction) Deterministic() bool                     { return w.declaration.Deterministic() }
 func (w *WindowFunction) NArgs() int                              { return len(w.args) }
 func (w *WindowFunction) Arg(i int) types.FuncArg                 { return w.args[i] }
+func (w *WindowFunction) FuncRef() uint32                         { return w.funcRef }
 func (w *WindowFunction) Phase() types.AggregationPhase           { return w.phase }
 func (w *WindowFunction) Invocation() types.AggregationInvocation { return w.invocation }
 func (w *WindowFunction) Decomposable() extensions.DecomposeType {
@@ -652,63 +653,6 @@ func (w *WindowFunction) Equals(other Expression) bool {
 	}
 
 	return true
-}
-
-func (w *WindowFunction) ToProto() *proto.Expression {
-	var (
-		args       []*proto.FunctionArgument
-		sorts      []*proto.SortField
-		parts      []*proto.Expression
-		upperBound *proto.Expression_WindowFunction_Bound
-		lowerBound *proto.Expression_WindowFunction_Bound
-	)
-
-	if len(w.args) > 0 {
-		args = make([]*proto.FunctionArgument, len(w.args))
-		for i, a := range w.args {
-			args[i] = a.ToProtoFuncArg()
-		}
-	}
-
-	if len(w.Sorts) > 0 {
-		sorts = make([]*proto.SortField, len(w.Sorts))
-		for i, s := range w.Sorts {
-			sorts[i] = s.ToProto()
-		}
-	}
-
-	if len(w.Partitions) > 0 {
-		parts = make([]*proto.Expression, len(w.Partitions))
-		for i, p := range w.Partitions {
-			parts[i] = p.ToProto()
-		}
-	}
-
-	if w.UpperBound != nil {
-		upperBound = w.UpperBound.ToProto()
-	}
-
-	if w.LowerBound != nil {
-		lowerBound = w.LowerBound.ToProto()
-	}
-
-	return &proto.Expression{
-		RexType: &proto.Expression_WindowFunction_{
-			WindowFunction: &proto.Expression_WindowFunction{
-				FunctionReference: w.funcRef,
-				Arguments:         args,
-				Options:           types.FunctionOptionsToProto(w.options),
-				OutputType:        types.TypeToProto(w.outputType),
-				Phase:             proto.AggregationPhase(w.phase),
-				Sorts:             sorts,
-				Invocation:        proto.AggregateFunction_AggregationInvocation(w.invocation),
-				Partitions:        parts,
-				BoundsType:        proto.Expression_WindowFunction_BoundsType(w.BoundsType),
-				LowerBound:        lowerBound,
-				UpperBound:        upperBound,
-			},
-		},
-	}
 }
 
 func (w *WindowFunction) ToProtoFuncArg() *proto.FunctionArgument {
--- expr/window_function_test.go
+++ expr/window_function_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/substrait-io/substrait-go/v9/expr"
 	"github.com/substrait-io/substrait-go/v9/extensions"
 	"github.com/substrait-io/substrait-go/v9/types"
+	"github.com/substrait-io/substrait-go/v9/wire"
 )
 
 func TestWindowFunctionBoundsType(t *testing.T) {
@@ -56,7 +57,7 @@ func TestWindowFunctionBoundsType(t *testing.T) {
 			require.NoError(t, err)
 
 			// Roundtrip: serialize and deserialize
-			protoExpr := wf.ToProto()
+			protoExpr := wire.ExprToProto(wf)
 			deserialized, err := expr.ExprFromProto(protoExpr, schema, reg)
 			require.NoError(t, err)
 
